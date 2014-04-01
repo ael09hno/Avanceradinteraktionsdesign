40ab39058accffb444d09ce1b7ee868d2ec98b71
ael09hno <henrik.normann@gmail.com>
2014-04-01 20:18:31 +0200

    Containing a new string for testing to commit

--- Flipit/gen/com/example/flipit/R.java
+++ Flipit/gen/com/example/flipit/R.java
@@ -46,6 +46,7 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int buttonstart_text=0x7f060004;
+        public static final int buttonstart_text2=0x7f060005;
         public static final int hello_world=0x7f060002;
         public static final int title_activity_main=0x7f060003;
     }
