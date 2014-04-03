d49af04d7f1a1f8fec2a8c3bd1756d5526e53b56
ael09hno <henrik.normann@gmail.com>
2014-04-03 11:31:58 +0200

    Eh commit
    no changes really....

--- Flipit/src/com/example/flipit/MainActivity.java
+++ Flipit/src/com/example/flipit/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.flipit;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		    
 	}
 
 	@Override
@@ -20,4 +26,14 @@ public class MainActivity extends Activity {
 		
 	}
 
+	
+	/*public void playsound(View view){
+		Intent intet = new Intent(this, MainActivity.class);
+	Button b = (Button) findViewById(R.id.button1);
+	b.
+	
+	}*/
+	
+	
+	
 }
